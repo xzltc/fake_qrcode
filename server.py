b87a7500b23e0b1d568cf3e16973881ac8c251ee
xzltc <xzhltc@gmail.com>
2021-05-06 23:16:32 +0800

    add seesion function

--- server.py
+++ server.py
@@ -5,13 +5,13 @@
 # @Software: PyCharm
 import datetime
 import os
-
 from flask import Flask, render_template, request, jsonify, redirect, url_for, session
 from flask_cors import *
 import json
 
 # Flask web端所有功能封装在server中
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(128)
 CORS(app, supports_credentials=True)
 
 
@@ -36,7 +36,23 @@ def submit():
         filehandle.close()
     # 模板渲染
     student = {'id': s_id, 'name': s_name, 'door': door, 'codeType': code_type}
+
+    session['studentID'] = s_id
+    session['studentName'] = s_name
+    session.permanent = True
+    app.permanent_session_lifetime = datetime.timedelta(days=14)
+
     return render_template('index_v2.html', s=student)
+
+
+@app.route('/checkSession')
+def get_session():
+    s_id = session.get('studentID')
+    s_name = session.get('studentName')
+    if s_id and s_name is not None:
+        return jsonify({'studentID': s_id, 'studentName': s_name})
+    else:
+        return jsonify({'error': 1})
 
 
 if __name__ == '__main__':
